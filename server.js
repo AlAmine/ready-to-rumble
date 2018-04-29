ba225d308e63eed8024bb6d0141012bd0916b50a
AlAmine <alamine.ousman@gmail.com>
2018-04-29 21:16:04 +0200

    Deploying index

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const md5 = require('MD5')
+const url = 'mongodb://nodeApp:567890@127.0.0.1:27017/memory';
+let DB;
 
 app.use(express.static('public'))
 
